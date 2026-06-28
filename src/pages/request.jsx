11d080f2ba4c9bc0521b084ff284c826e2fce431
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-06-28 04:39:54 +0200

    Fix redirect to request status page

--- src/pages/request.jsx
+++ src/pages/request.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function BeatmapRequestPage() {
+    const navigate = useNavigate();
     const [requests, setRequests] = useState([{ id: crypto.randomUUID() }]);
 
     function addRequest() {
