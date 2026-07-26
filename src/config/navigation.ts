d91782ca6dbe758ae5a089306228cf75408a89c6
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-07-26 15:49:45 +0200

    Add Navbar config

---
+++ src/config/navigation.ts
@@ -0,0 +1,23 @@
+export default [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "Beatmaps",
+    url: "/search",
+  },
+  {
+    title: "Request",
+    children: [
+      {
+        title: "Request Beatmap",
+        url: "/request",
+      },
+      {
+        title: "View Requests",
+        url: "/requests",
+      },
+    ],
+  },
+];
