2470047fa5d8cd58c905f76a1ac13c7763421f57
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-07-26 16:37:41 +0200

    Add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -32,4 +34,6 @@ export default defineConfig({
       },
     },
   },
+
+  integrations: [react()],
 });
