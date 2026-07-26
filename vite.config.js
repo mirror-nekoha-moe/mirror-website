6c04a65a9cd1119636c02a97daa4928f71b8ddf4
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-07-26 04:08:36 +0200

    some indent cleanup

--- vite.config.js
+++ vite.config.js
@@ -2,35 +2,35 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['import', 'global-builtin', 'color-functions'],
-      },
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: ['import', 'global-builtin', 'color-functions'],
+            },
+        },
     },
-  },
-  define: {
-    __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
-  },
-  resolve: {
-    alias: {
-      '~bootstrap': '/node_modules/bootstrap',
-    }
-  },
-  build: {
-    minify: 'esbuild',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      }
-    }
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:30727',
+    define: {
+        __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
     },
-  }
+    resolve: {
+        alias: {
+            '~bootstrap': '/node_modules/bootstrap',
+        }
+    },
+    build: {
+        minify: 'esbuild',
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`,
+            }
+        }
+    },
+    server: {
+        proxy: {
+            '/api': 'http://localhost:30727',
+        },
+    }
 })
